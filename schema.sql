b4fa1a5031dc17d211d380ec730a8dcbd668e4de
Hector <vv96rca@gmail.com>
2021-11-30 16:09:21 +0530

    using gitflow

--- schema.sql
+++ schema.sql
@@ -1,11 +1,2 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals(
-   id   INT NOT NULL,
-   name VARCHAR (100) NOT NULL,
-   date_of_birth  DATE NOT NULL,
-   escape_attempts  INT ,
-   neutered   BOOLEAN,
-   weight_kg  DECIMAL,
-   PRIMARY KEY (id)
-);
