7147f544f155331c98961a43c141ace3e33db2ec
Hector <vv96rca@gmail.com>
2021-11-30 16:06:55 +0530

    added schema , data and queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT * FROM animals WHERE   date_of_birth > '01/01/2016' AND date_of_birth   <= '31/12/2019';
+SELECT * FROM animals WHERE   escape_attempts < '3' AND neutered = true
+SELECT date_of_birth from animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT NAME, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE NOT SELECT * FROM animals WHERE   escape_attempts < '3' AND neutered = true;
+SELECT * FROM animals WHERE NAME != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
