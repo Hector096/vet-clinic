37aa3e7cc76e87e5248bc02c78c8172e1ab64397
Hector <vv96rca@gmail.com>
2021-11-30 16:09:59 +0530

    added schema , data and queries

--- schema.sql
+++ schema.sql
@@ -1,2 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE TABLE animals(
+   id   INT NOT NULL,
+   name VARCHAR (100) NOT NULL,
+   date_of_birth  DATE NOT NULL,
+   escape_attempts  INT ,
+   neutered   BOOLEAN,
+   weight_kg  DECIMAL,
+   PRIMARY KEY (id)
+);
